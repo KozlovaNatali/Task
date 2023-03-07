9bf43758e92335e15f8ea2dbfa05e2549f332349
KozlovaNatali <sipporte169523@gmail.com>
2023-03-07 19:30:33 +0300

    Task 07 - выполнено

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num >= 100 && num <= 999)
+{
+    int numLast = num % 10;
+    Console.WriteLine(numLast);
+}
+else
+{
+   Console.WriteLine("Введите ТРЁхзначное число"); 
+}
