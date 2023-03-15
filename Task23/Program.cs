b9054824ff79c375bbbab1d54bb8b23e4cc7e29c
KozlovaNatali <sipporte169523@gmail.com>
2023-03-15 10:57:56 +0300

    Task23 - выполнено

---
+++ Task23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+TableCubes(num);
+
+void TableCubes (int number)
+{
+    int count = 1;
+    while(count <= number)
+    {
+        int squad = count * count * count;
+        Console.WriteLine($"{count, 3} -> {squad, 8}");
+        count ++;
+    }
+}
