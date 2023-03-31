1f1dcd2a5fb4ec8ba066ad1fdc85f4bb78171b25
KozlovaNatali <sipporte169523@gmail.com>
2023-03-31 18:36:37 +0300

    Task 55 - выполнено

---
+++ Task55/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+int[,] array2d = CreateMatrixRndInt(5, 4, -100, 100);
+if(array2d.GetLength(0) == array2d.GetLength(1))
+{
+    ReplacingRowsWithColumns(array2d);
+}
+else
+{
+    Console.WriteLine("Неверный массив");
+}
+PrintMatrix(array2d);
+Console.WriteLine();
+int[,] newMatrix = ReplacingRowsWithColumns(array2d);
+PrintMatrix(newMatrix);
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int [,] ReplacingRowsWithColumns(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    int[,] newMatrix = new int[rows, columns];
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            newMatrix[i, j] = matrix[j , i];
+        }
+    }
+    return newMatrix;
+}
