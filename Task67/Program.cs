eb6b0e6574826bdd63031a89fb40aea2f97d3a31
KozlovaNatali <sipporte169523@gmail.com>
2023-04-04 21:05:07 +0300

    Task 67 выполнено

---
+++ Task67/Program.cs
@@ -0,0 +1,23 @@
+﻿// int FactorialRec(int n)
+//  {
+//      if (n == 1) return 1;
+//      else return n * FactorialRec(n - 1);
+//  }
+
+//  Console.WriteLine(FactorialRec(10)); // 3628800
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+Console.WriteLine($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumDigirs(number);
+Console.WriteLine($"Сумма чисел в цифре равна {sum}");
+
+int SumDigirs( int num)
+{
+    if (num == 0) return 0;
+    else return SumDigirs(num / 10) + num % 10;
+}
